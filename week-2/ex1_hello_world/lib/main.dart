5a23292430a3878d9ff10e735eaa7730facf3b8e
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-01-17 09:30:34 +0700

    Update Week 2

--- week-2/ex1_hello_world/lib/main.dart
+++ week-2/ex1_hello_world/lib/main.dart
@@ -7,46 +7,69 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
     return MaterialApp(
-      title: '6288087',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
+      title: 'iamPrompt Hello World',
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.red,
+          secondary: Colors.blue,
+        ),
       ),
-      home: const MyHomePage(title: '6288087'),
+      home: const MyHomePage(title: 'iamPrompt Hello World'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Padding(
-              padding: EdgeInsets.only(bottom: 16),
-              child: Text(
-                'Supakarn Laorattanakul',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Text('My hobbies is coding.', style: TextStyle(fontSize: 24)),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Hello World',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                '6288087 Supakarn Laorattanakul',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                '6288123 Thanaboon Sapmontree',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
